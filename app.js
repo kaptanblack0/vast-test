0af0632d316d27352ccf7862c4f04ba26a294779
kaptanblack0 <mrcankrks@gmail.com>
2025-11-11 01:03:13 +0300

    yoruldum

--- app.js
+++ app.js
@@ -8,37 +8,28 @@ const AD_VIDEO_URL = "https://ads.amfipark.com/bir.mp4";
 
 // VAST XML üretimi
 app.get("/vast.xml", (req, res) => {
-  const xml = `
-  <?xml version="1.0" encoding="UTF-8"?>
-  <VAST version="3.0">
-    <Ad id="1">
-      <InLine>
-        <AdSystem>SimpleNodeVAST</AdSystem>
-        <AdTitle>My Ad</AdTitle>
-        <Impression><![CDATA[https://example.com/track/impression]]></Impression>
-        <Creatives>
-          <Creative>
-            <Linear>
-              <Duration>00:00:04</Duration>
-              <MediaFiles>
-                <MediaFile
-                  delivery="progressive"
-                  type="video/mp4"
-                  width="1920"
-                  height="1080"
-                  scalable="true"
-                  maintainAspectRatio="true"
-                >
-                  <![CDATA[${AD_VIDEO_URL}]]>
-                </MediaFile>
-              </MediaFiles>
-            </Linear>
-          </Creative>
-        </Creatives>
-      </InLine>
-    </Ad>
-  </VAST>
-  `;
+ const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<VAST version="3.0">
+  <Ad id="1">
+    <InLine>
+      <AdSystem>SimpleNodeVAST</AdSystem>
+      <AdTitle>My Ad</AdTitle>
+      <Impression><![CDATA[https://example.com/track/impression]]></Impression>
+      <Creatives>
+        <Creative>
+          <Linear>
+            <Duration>00:00:04</Duration>
+            <MediaFiles>
+              <MediaFile delivery="progressive" type="video/mp4" width="1920" height="1080" scalable="true" maintainAspectRatio="true">
+                <![CDATA[${AD_VIDEO_URL}]]>
+              </MediaFile>
+            </MediaFiles>
+          </Linear>
+        </Creative>
+      </Creatives>
+    </InLine>
+  </Ad>
+</VAST>`;
 
   res.set("Content-Type", "application/xml");
   res.send(xml);
