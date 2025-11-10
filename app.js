88a7c93bd63241643b646a17bdd695676277fabe
kaptanblack0 <mrcankrks@gmail.com>
2025-11-11 02:00:47 +0300

    yoruldum1212312312123343

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Doğrudan MP4 video URL'i kullanın (embed URL değil!)
 // Bunny Stream'den videonun doğrudan MP4 linkini alın
-const AD_VIDEO_URL = "https://vz-7335a66b-be3.b-cdn.net/dee86846-9223-4695-9ba5-42bea20c47db/playlist.m3u8";
+const AD_VIDEO_URL = "https://ads.amfipark.com/bir.mp4";
 
 // CORS ayarları (reklam player'lar için gerekli)
 app.use((req, res, next) => {
